4112d330247d5d3b9c16d5ded822481600b4f659
tnsy <soo5717@sookmyung.ac.kr>
2021-10-27 00:52:36 +0900

    feat(all): articles, ploggings, cheering repository 추가

--- src/articles/articles.module.ts
+++ src/articles/articles.module.ts
@@ -1,9 +1,13 @@
+import { Article } from './entities/article.entity';
 import { Module } from '@nestjs/common';
 import { ArticlesService } from './articles.service';
 import { ArticlesController } from './articles.controller';
+import { TypeOrmModule } from '@nestjs/typeorm';
 
 @Module({
+  imports: [TypeOrmModule.forFeature([Article])],
   controllers: [ArticlesController],
   providers: [ArticlesService],
+  exports: [TypeOrmModule],
 })
 export class ArticlesModule {}
--- src/cheering/cheering.service.ts
+++ src/cheering/cheering.service.ts
@@ -1,9 +1,17 @@
+import { Cheering } from './entities/cheering.entity';
 import { Injectable } from '@nestjs/common';
 import { CreateCheeringDto } from './dto/create-cheering.dto';
 import { UpdateCheeringDto } from './dto/update-cheering.dto';
+import { Repository } from 'typeorm';
+import { InjectRepository } from '@nestjs/typeorm';
 
 @Injectable()
 export class CheeringService {
+  constructor(
+    @InjectRepository(Cheering)
+    private cheeringRepository: Repository<Cheering>,
+  ) {}
+
   create(createCheeringDto: CreateCheeringDto) {
     return 'This action adds a new cheering';
   }
