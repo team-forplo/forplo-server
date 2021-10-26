2dd0ccaf09a537d68d5f8145d30d8c8da6f0fa6e
tnsy <soo5717@sookmyung.ac.kr>
2021-10-27 03:37:53 +0900

    feat(dto): CreatePloggingDto 생성

--- src/ploggings/dto/create-plogging.dto.ts
+++ src/ploggings/dto/create-plogging.dto.ts
@@ -1,1 +1,10 @@
-export class CreatePloggingDto {}
+import { Plogging } from './../entities/plogging.entity';
+import { PickType } from '@nestjs/swagger';
+export class CreatePloggingDto extends PickType(Plogging, [
+  'location',
+  'distance',
+  'time',
+  'imageUrl',
+  'memo',
+  'isPublic',
+] as const) {}
