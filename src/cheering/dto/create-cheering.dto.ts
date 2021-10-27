88c62506e63bc865b4fbcacfaf39237a99f5bca5
tnsy <soo5717@sookmyung.ac.kr>
2021-10-28 03:45:08 +0900

    feat(dto): CreateCheeringDto 생성

--- src/cheering/dto/create-cheering.dto.ts
+++ src/cheering/dto/create-cheering.dto.ts
@@ -1,1 +1,14 @@
-export class CreateCheeringDto {}
+import { ApiProperty } from '@nestjs/swagger';
+import { IsDefined } from 'class-validator';
+import { Column } from 'typeorm';
+
+export class CreateCheeringDto {
+  @ApiProperty({
+    example: 1,
+    description: '플로깅 아이디',
+    required: true,
+  })
+  @Column()
+  @IsDefined()
+  ploggingId: number;
+}
