1e9031c56b37e33da2fd738522d98b961053802e
tnsy <soo5717@sookmyung.ac.kr>
2021-10-30 21:05:34 +0900

    feat(service): 국내여행 상세 정보 조회 구현

--- src/search/search.service.ts
+++ src/search/search.service.ts
@@ -1,6 +1,5 @@
 import { Injectable } from '@nestjs/common';
 import axios from 'axios';
-
 @Injectable()
 export class SearchService {
   private readonly BASE_URL =
@@ -19,6 +18,31 @@ export class SearchService {
     return `${year}${formatMonth}${formatDate}`;
   }
 
+  getFormatEstiDecoDivCd(estiDecoDivCd: number) {
+    let formatEstiDecoDivCd;
+    switch (estiDecoDivCd) {
+      case 1:
+        formatEstiDecoDivCd = '쾌적';
+        break;
+      case 2:
+        formatEstiDecoDivCd = '여유';
+        break;
+      case 3:
+        formatEstiDecoDivCd = '보통';
+        break;
+      case 4:
+        formatEstiDecoDivCd = '약간혼잡';
+        break;
+      case 5:
+        formatEstiDecoDivCd = '혼잡';
+        break;
+      default:
+        formatEstiDecoDivCd = '혼잡';
+        break;
+    }
+    return formatEstiDecoDivCd;
+  }
+
   // 지역 기반 관광 정보 조회
   async areaBasedList(params: any) {
     const areaBasedList = await axios.get(
@@ -28,6 +52,17 @@ export class SearchService {
       },
     );
     return areaBasedList.data.response.body.items.item || {};
+  }
+
+  // 키워드 검색 조회
+  async searchKeyword(params: any) {
+    const searchKeyword = await axios.get(
+      `${this.BASE_URL}/KorService/searchKeyword?${this.COMMON_QUERY}`,
+      {
+        params,
+      },
+    );
+    return searchKeyword.data.response.body.items.item || {};
   }
 
   // 공통정보 조회
@@ -60,9 +95,11 @@ export class SearchService {
         params,
       },
     );
-    return tarDecoList.data.response.body.items.item || {};
+    const { estiDecoDivCd } = tarDecoList.data.response.body.items.item || {};
+    return this.getFormatEstiDecoDivCd(estiDecoDivCd);
   }
 
+  // 오늘의 추천 여행지 조회
   async findTopList() {
     // 지역 기반 관광 정보 조회 (조회순)
     let areaBaseItemList = await this.areaBasedList({
@@ -76,29 +113,14 @@ export class SearchService {
     const sliceStart = (todayDate - 1) * 10;
     areaBaseItemList = areaBaseItemList.slice(sliceStart, sliceStart + 10);
 
-    // 개요 및 상세 정보 포함한 목록
+    // 개요 포함한 목록
     return await Promise.all(
       areaBaseItemList.map(async (item) => {
-        const { contentid, title, addr1, firstimage, firstimage2, mapx, mapy } =
-          item || {};
+        const { contentid, title, addr1, firstimage, firstimage2 } = item || {};
 
-        const { contenttypeid, overview } = await this.detailCommon({
+        const { overview } = await this.detailCommon({
           contentId: contentid,
           overviewYN: 'Y',
-        });
-
-        const { infocenter } = await this.detailIntro({
-          contentId: contentid,
-          contentTypeId: contenttypeid,
-        });
-
-        const today = this.getFormatDate();
-        const { estiDecoDivCd } = await this.tarDecoList({
-          numOfRows: 1,
-          pageNo: 1,
-          startYmd: today,
-          endYmd: today,
-          contentId: contentid,
         });
 
         return {
@@ -107,21 +129,58 @@ export class SearchService {
           addr1,
           firstimage,
           firstimage2,
-          infocenter,
-          mapx,
-          mapy,
           overview,
-          estiDecoDivCd,
         };
       }),
     );
   }
 
+  // 국내 여행 상세 조회 (북마크 관련 추가 필요)
   async findDetail(contentid: number) {
-    return await this.areaBasedList({
-      numOfRows: 310,
-      arrange: 'P',
-      contentTypeId: 12,
+    const {
+      contenttypeid,
+      title,
+      firstimage,
+      firstimage2,
+      addr1,
+      mapx,
+      mapy,
+      overview,
+    } = await this.detailCommon({
+      contentId: contentid,
+      defaultYN: 'Y',
+      firstImageYN: 'Y',
+      areacodeYN: 'Y',
+      addrinfoYN: 'Y',
+      mapinfoYN: 'Y',
+      overviewYN: 'Y',
     });
+
+    const { infocenter } = await this.detailIntro({
+      contentId: contentid,
+      contentTypeId: contenttypeid,
+    });
+
+    const today = this.getFormatDate();
+    const estiDecoDivCd = await this.tarDecoList({
+      numOfRows: 1,
+      pageNo: 1,
+      startYmd: today,
+      endYmd: today,
+      contentId: contentid,
+    });
+
+    return {
+      contentid,
+      title,
+      firstimage,
+      firstimage2,
+      addr1,
+      infocenter,
+      mapx,
+      mapy,
+      overview,
+      estiDecoDivCd,
+    };
   }
 }
