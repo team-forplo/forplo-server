f24c5f89ee5ff0833a75e22147911ddaae76eecc
tnsy <soo5717@sookmyung.ac.kr>
2021-10-31 12:22:31 +0900

    fix(module): Search - Bookmark 순환 참조 문제 해결

--- src/bookmark/bookmark.module.ts
+++ src/bookmark/bookmark.module.ts
@@ -1,11 +1,15 @@
+import { SearchModule } from './../search/search.module';
 import { Bookmark } from 'src/bookmark/entities/bookmark.entity';
-import { Module } from '@nestjs/common';
+import { forwardRef, Module } from '@nestjs/common';
 import { BookmarkService } from './bookmark.service';
 import { BookmarkController } from './bookmark.controller';
 import { TypeOrmModule } from '@nestjs/typeorm';
 
 @Module({
-  imports: [TypeOrmModule.forFeature([Bookmark])],
+  imports: [
+    TypeOrmModule.forFeature([Bookmark]),
+    forwardRef(() => SearchModule),
+  ],
   controllers: [BookmarkController],
   providers: [BookmarkService],
   exports: [TypeOrmModule, BookmarkService],
--- src/search/search.module.ts
+++ src/search/search.module.ts
@@ -1,12 +1,13 @@
 import { BookmarkModule } from './../bookmark/bookmark.module';
 import { SearchService } from './search.service';
 import { SearchController } from './search.controller';
-import { Module } from '@nestjs/common';
+import { forwardRef, Module } from '@nestjs/common';
 import { ConfigModule } from '@nestjs/config';
 
 @Module({
-  imports: [ConfigModule.forRoot(), BookmarkModule],
+  imports: [ConfigModule.forRoot(), forwardRef(() => BookmarkModule)],
   controllers: [SearchController],
   providers: [SearchService],
+  exports: [SearchService],
 })
 export class SearchModule {}
